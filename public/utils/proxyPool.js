63bf4a9c8ce84bc0443c476eff4a58613330a733
UncleYee <uncleye159@gmail.com>
2017-12-14 22:43:40 +0800

    IP代理池

--- public/utils/proxyPool.js
+++ public/utils/proxyPool.js
@@ -2,8 +2,8 @@ const request = require("request");
 const iconv = require('iconv-lite');
 
 function getProxyList() {
-    var apiURL = 'http://www.66ip.cn/mo.php?sxb=&tqsl=100&port=&export=&ktip=&sxa=&submit=%CC%E1++%C8%A1&textarea=http%3A%2F%2Fwww.66ip.cn%2F%3Fsxb%3D%26tqsl%3D100%26ports%255B%255D2%3D%26ktip%3D%26sxa%3D%26radio%3Dradio%26submit%3D%25CC%25E1%2B%2B%25C8%25A1';
-
+    var apiURL = 'http://www.66ip.cn/mo.php?sxb=&tqsl=10&port=&export=&ktip=&sxa=&submit=%CC%E1++%C8%A1&textarea=http%3A%2F%2Fwww.66ip.cn%2F%3Fsxb%3D%26tqsl%3D100%26ports%255B%255D2%3D%26ktip%3D%26sxa%3D%26radio%3Dradio%26submit%3D%25CC%25E1%2B%2B%25C8%25A1';
+    var httpsUrl = 'http://www.66ip.cn/nmtq.php?getnum=&isp=0&anonymoustype=0&start=&ports=&export=&ipaddress=&area=0&proxytype=1&api=66ip';
     return new Promise((resolve, reject) => {
         var options = {
             method: 'GET',
